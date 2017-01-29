804e742e2d54028617b665144d50c42ea8cdc68e
KyKri <krkrick@gmail.com>
2017-01-29 15:55:56 -0800

    Added sign up section

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
     $("#learn-more").hide()
+    $("#sign-up").hide()
     $("#learn").click(function(){
         $("#learn-more").slideDown('slow');
+    });
+    $("#sign-me-up").click(function(){
+        $("#sign-up").slideDown('slow');
     });
 });
