4cfb62f3204a42306cda76d8a9e30c1a7d0f947a
KyKri <krkrick@gmail.com>
2017-01-29 16:11:45 -0800

    Added some functionality to sign up section

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
     $("#learn-more").hide()
     $("#sign-up").hide()
+
     $("#learn").click(function(){
         $("#learn-more").slideDown('slow');
     });
+
     $("#sign-me-up").click(function(){
         $("#sign-up").slideDown('slow');
+    });
+
+    $(".submit").click(function(){
+        $("input[name='Email']").val("");
+        $("input[name='Pass']").val("");
+        alert("Thanks for signing up! You should receive a confirmation email soon!");
     });
 });
